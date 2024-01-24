4960c3b073b984136927d7e5cfadedc35e76e982
jonathan <jonathan.luscap@le-campus-numerique.fr>
2024-01-24 12:18:27 +0100

    Mise en place du FrontController et architecture logiciel

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
-echo "Bienvenu sur le blog de ouf ! ";
+session_start();
+if (!isset($_SESSION["countViewPage"])){
+    $sessionDate = new DateTime();
+    $_SESSION['date'] = $sessionDate->format('Y-m-d-H-i-s');
+}
+require_once 'ressources/views/layouts/header.php'; ?>
+
+<main class="container">
+
+    <?php
+    $page = filter_input(INPUT_GET, 'action',FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+</main>
+
+<?php require_once 'ressources/views/layouts/footer.php' ?>
