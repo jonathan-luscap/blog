4edfe6fcc2cff58b3157bc7bedaa61805ddd882a
jonathan <jonathan.luscap@le-campus-numerique.fr>
2024-01-24 14:41:04 +0100

    ajout homeControler.php et maj index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
-session_start();
-if (!isset($_SESSION["countViewPage"])){
-    $sessionDate = new DateTime();
-    $_SESSION['date'] = $sessionDate->format('Y-m-d-H-i-s');
+
+$routerArray = [
+        'homeController'
+];
+
+$page = filter_input(INPUT_GET, 'action',FILTER_SANITIZE_SPECIAL_CHARS);
+
+foreach ($routerArray as $value) {
+    if ($value === $page) {
+        require_once '../app/controllers/'.$page;
+    } else {
+        require_once '../app/controllers/homeController.php';
+    }
 }
-require_once 'ressources/views/layouts/header.php'; ?>
-
-<main class="container">
-
-    <?php
-    $page = filter_input(INPUT_GET, 'action',FILTER_SANITIZE_SPECIAL_CHARS);
-
-
-</main>
-
-<?php require_once 'ressources/views/layouts/footer.php' ?>
+?>
